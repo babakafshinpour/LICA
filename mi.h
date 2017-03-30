7f0fb9193c3c3fd6d98c426c247175b4a3d7e510
babak <babak@rtdsinc.com>
2017-03-30 12:49:41 -0400

    uploaded

--- mi.h
+++ mi.h
@@ -1,2 +1,22 @@
+// Copyright 2017 Babak Afshin-Pour
+//-----------------------------------------------------------------------------------------
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should receive a copy of the GNU General Public License
+// along with this program.  See also <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------------------- 
+// Contacts:
+//
+// babak.afshinpour@gmail.com
+//-----------------------------------------------------------------------------------------
+
 double mixnyn(double *rangevec, int n, int c, int dimx, int k);
 void mlica(double* inp2, int N, int dim, int K, int method, int nr, int ns, double addnoise, double **arr_out, int *dim1_out, int *dim2_out);
